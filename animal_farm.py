beb75ffcca71882049f654e0ccafdefd5934cc86
YushiNishikawa <yushi_nishikawa@geosurf.net>
2020-11-11 13:23:26 +0900

    Added a lion to the animal farm

--- animal_farm.py
+++ animal_farm.py
@@ -7,6 +7,7 @@ import sys
 from animals import cat
 from animals import dog
 from animals import sheep
+from animals import lion
 import animal
 import farm
 
@@ -16,6 +17,8 @@ def make_animal(kind):
         return cat.Cat()
     if kind == 'dog':
         return dog.Dog()
+    if kind == 'lion'
+        return lion.Lion()
     if kind == 'sheep':
         return sheep.Sheep()
     return animal.Animal(kind)
